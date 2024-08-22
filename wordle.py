d6cc71817acb8895b618d89663d4ae9e3f513776
eli descallar <elidescallar01@gmail.com>
2024-08-22 17:39:31 +0000

    complete wordle

--- wordle.py
+++ wordle.py
@@ -1,29 +1,21 @@
 # TASK A: Define a variable 'word' that holds the correct word for the wordle game
-word = "CRATE"
+word = "crate"
 
 # TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
 
 def makeAGuess(guess):
 
-  
-
-  
-
-
-
-
-
   # TASK C:Define a variable 'hint' that holds an empty string
   hint = ""
 
   # TASK D: Build a loop that loops from 0 to the length of word
-  for i in range(5):
+  for i in range(len(word)):
 
     if guess[i] == word[i]:
-      hint += "G"
+      hint += "g"
 
     elif guess[i] in word:
-      hint += "Y"
+      hint += "y"
     
     else:
       hint += "-"
@@ -47,8 +39,21 @@ def makeAGuess(guess):
 
 print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a valid 5-letter word. For each guess, a hint will tell you how many letters you've guessed correctly. A G represents a letter in the word and in the correct spot.. A Y represents a letter in the word but in the wrong spot. A - represents a letter not in the word in any spot. \n Guess below! \n")
 
-# TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)
+# TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)#
+for i in range(6):
+  guess = input("guess a 5-letter word ")
+  hint = makeAGuess(guess)
+  print(hint)
+  if hint == "ggggg":
+    print("You Win")
+    break
 
+
+
+if hint != "ggggg":
+  print("You Lose")
+
+print(hint)
 
   # TASK J: Define a variable 'guess'. prompt the user for their 5-letter guess and store it in the variable
 
@@ -62,4 +67,3 @@ print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a v
 # TASK N: After the loop has finished, meaning the user has run out of guesses, check if hint != "GGGGG". If so, the user has lost. Print a lose message. 
 
 
-print(makeAGuess("CLEAR"))
