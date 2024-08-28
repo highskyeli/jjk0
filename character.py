39e459cf91c1b3d64a19fb86d38e439d9bf1b53d
eli descallar <elidescallar01@gmail.com>
2024-08-28 18:00:06 +0000

    v1 finished

--- character.py
+++ character.py
@@ -1,1 +1,27 @@
+class Character:
+"""
+    Represents a character in the game.
+    
+    Attributes:
+        name (str): The name of the character.
+        health (int): The current health of the character.
+        inventory (list): The items in the character's inventory.
+        dialogue (dict): The dialogue responses for the character.
+    
+    Methods:
+        talk_to_player(player): Prints the appropriate dialogue response based on the player's message.
+        receive_item(item): Adds the given item to the character's inventory.
+    """
+        def __init__(self, name, health, inventory):
+        self.name = name
+        self.health = health
+        self.inventory = inventory
+        self.dialogue = dialogue
 
+    def talk_to_player(self, player):
+        for sentence in self.dialogue:
+            if player_msg == sentence:
+                print(self.dialogue[sentence])
+
+    def receive_item(self, item):
+        self.inventory.append(item) 
--- runner.py
+++ runner.py
@@ -1,1 +1,12 @@
+import Character from character
+import Item from item
+import playWordle from wordle
 
+print("Welcome to the game!")
+d1 = input("You come up to a field and see a star! Do you want to pick it up? (y/n)")
+if d1 == "y":
+    star = Item("Star", "A shiny star", 10)
+    dla = input("You pick up the star and it shines brightly. Now you see a dude. Want to talk to him? (y/n)")
+    if dla == "y":
+        character = Character("Dude", 100, [], {"Hello": "Hello there!"})
+        character.talk_to_player("Hello")
